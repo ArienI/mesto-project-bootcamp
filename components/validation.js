30afc06fae233190c3616cc5f61d659a519b5c2a
Arien <Laurilin2022@gmail.com>
2023-11-20 01:44:11 +0100

    feat: добавление валидации

---
+++ components/validation.js
@@ -0,0 +1,89 @@
+// форма редактирования профиля
+// const form = document.forms['formEditProfile']
+// console.log(form);
+
+// функция, которая прячет ошибку
+function hideError(inputField, validationConfig) {
+  const spanId = 'error-' + inputField.id;
+  const spanElement = document.getElementById(spanId);
+  spanElement.textContent = '';
+  spanElement.textContent = errorMessage;
+  inputField.classList.remove(validationConfig.inputInvalidClass)
+}
+
+// функция, которая показывает ошибку
+function showError(inputField, errorMessage, validationConfig) {
+  const spanId = 'error-' + inputField.id;
+  const spanElement = document.getElementById(spanId);
+  spanElement.textContent = errorMessage;
+  inputField.classList.add(validationConfig.inputInvalidClass)
+}
+
+//проверяем ошибки
+function handleInput(inputElement, validationConfig) {
+  // const inputElement = evt.target;
+  // console.log(inputElement.validity);
+  if (inputElement.validity.valid) {
+    // функция, которая прячет ошибку
+    hideError(inputElement, validationConfig);
+    // console.log('Валидно');
+  } else {
+    // функция, которая показывает ошибку
+    showError(inputElement, inputElement.validationMessage, validationConfig);
+    // console.log('ошибка в поле:', inputElement.validationMessge);
+  }
+}
+
+// получаем кнопку для включения
+function enableButton(button) {
+  // disabled- атрибут делающий кнопку неактивной
+  button.disabled = false
+}
+
+// получаем кнопку для выключения
+export function disableButton(button) {
+  // disabled- атрибут делающий кнопку неактивной
+  button.disabled = true
+}
+
+// вкл/выкл кнопки
+function checkForm(form, button) {
+  // checkValidity- метод возвращает true, если вся-вся форма валидна
+  if (form.checkValidity()) {
+    enableButton(button) // включить
+  } else {
+    disableButton(button) //выключить
+  }
+}
+
+
+//функция очистки формы при перезвгрузке
+export function resetError(form, validationConfig) {
+  const inputList = form.querySelectorAll(validationConfig.inputSelector); // нашли все input
+  const submitButton = form.querySelector(validationConfig.submitSelector); // нашли кнопку (/и?)
+  checkForm(form, submitButton); // для того, что бы при перезагрузке страницы кнопка сразу была не активной
+  inputList.forEach(input => {
+    //установили слушатель
+    hideError(input, validationConfig);
+  });
+};
+
+
+
+// функция активирующая валидацию всех форм нашего приложения
+export function enableValidation(validationConfig) {
+  // найдем все формы
+  const formList = document.querySelectorAll(validationConfig.formSelector);
+  formList.forEach(form => {
+    const inputList = form.querySelectorAll(validationConfig.inputSelector); // нашли все input
+    const submitButton = form.querySelector(validationConfig.submitSelector); // нашли кнопку (/и?)
+    checkForm(form, submitButton); // для того, что бы при перезагрузке страницы кнопка сразу была не активной
+    inputList.forEach(input => {
+      //установили слушатель
+      input.addEventListener('input', () => {
+        handleInput(input, validationConfig);
+        checkForm(form, submitButton)
+      });
+    });
+  })
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,4 @@
-const initialCards = [
-  {
-    name: 'водичка)',
-    link: 'https://images.unsplash.com/photo-1503756234508-e32369269deb?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: '="._."=',
-    link: 'https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2F0fGVufDB8fDB8fHww'
-  },
-  {
-    name: 'мухомор',
-    link: 'https://images.unsplash.com/photo-1605874575508-504999dcb8fb?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'ягодки',
-    link: 'https://images.unsplash.com/photo-1667285435809-20941120f9bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fCVEMCVCQiVEMCVCNSVEMSU4MXxlbnwwfHwwfHx8MA%3D%3D'
-  },
-  {
-    name: '...',
-    link: 'https://images.unsplash.com/photo-1670946789981-6e86ae8d0f43?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fCVEMCVCRiVEMCVCMCVEMCVCQiVEMSU4QyVEMCVCQyVEMCVCMHxlbnwwfHwwfHx8MA%3D%3D'
-  },
-  {
-    name: 'meow',
-    link: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0fGVufDB8fDB8fHww'
-  },
-  {
-    name: 'лошадка',
-    link: 'https://images.unsplash.com/photo-1577936861999-2ee541936e4b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'ещё одна лошадка',
-    link: 'https://images.unsplash.com/photo-1539808163380-beb256654c64?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'замок',
-    link: 'https://images.unsplash.com/photo-1514539079130-25950c84af65?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'озеро',
-    link: 'https://images.unsplash.com/photo-1505159940484-eb2b9f2588e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'flowers',
-    link: 'https://images.unsplash.com/photo-1490709501740-c7ac36b7d587?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'this was a Hobbit hole',
-    link: 'https://images.unsplash.com/photo-1575735409309-e0ecb6088fcd?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: ')',
-    link: 'https://plus.unsplash.com/premium_photo-1668723712387-d5076dae388e?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'boo...',
-    link: 'https://images.unsplash.com/photo-1635604521676-04f4f46b60e2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'тыктыктыковка',
-    link: 'https://images.unsplash.com/photo-1573051056354-ac3efd32cd67?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: '(((',
-    link: 'https://images.unsplash.com/photo-1699804368701-d52e876bf01a?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-];
+import { initialCards } from './utils/utils.js';
 
 // кнопка редактировать профиль
 const editProfileButton = document.querySelector(".profile__button-edit");
@@ -245,3 +180,24 @@ formAddCard.addEventListener('submit', (evt) => {
   // Закрываем попап
   closePopup(popupAddCard);
 });
+
+
+import { enableValidation, disableButton, resetError } from "../components/validation.js";
+
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitSelector: '.popup__button',
+  // красное подчеркивание
+  inputInvalidClass: 'popup__input_invalid',
+}
+
+enableValidation(validationConfig);
+
+/*отключение кнопки
+function handleSubmit(evt) {
+  formAddCard.reset()
+  disableButton(evt.submitter)
+}
+
+formAddCard.addEventListener('submit', handleSubmit)*/
